f7949af03a0fb56463ecdb478ce6fb310b000582
Skatetablet <skatewer@gmail.com>
2023-06-21 21:15:59 -0700

    Actualiza categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,7 +4,7 @@ import { AddCategory, GifGrid } from './components';
 const GifExpertApp = () => {
 
 
-  const [categories, setCategories] = useState(["Seahawks"]);
+  const [categories, setCategories] = useState([]);
 
   const onAddCategory = (newCategory) => {
     //Es una opcion
